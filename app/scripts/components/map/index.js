d2f02b1c1860e88ee388538d3422cc7e4b9482de
Derek Lieu <dl@dereklieu.com>
2017-02-27 12:02:07 +0000

    Move logic to determine if mapbox in browser to util

--- app/scripts/components/map/index.js
+++ app/scripts/components/map/index.js
@@ -1,16 +1,15 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
-import mapboxgl from 'mapbox-gl';
+import { mapboxgl } from '../../util/window';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwZWd5cHQiLCJhIjoiY2l6ZTk5YTNxMjV3czMzdGU5ZXNhNzdraSJ9.HPI_4OulrnpD8qI57P12tg';
 const id = 'main-map-component';
-
 const Map = React.createClass({
   displayName: 'Map',
 
   initMap: function (el) {
     if (el && !this.map) {
+      mapboxgl.accessToken = 'pk.eyJ1IjoibWFwZWd5cHQiLCJhIjoiY2l6ZTk5YTNxMjV3czMzdGU5ZXNhNzdraSJ9.HPI_4OulrnpD8qI57P12tg';
       this.map = new mapboxgl.Map({
         center: [125.48, 9.7],
         container: el,
--- app/scripts/util/window.js
+++ app/scripts/util/window.js
@@ -1,5 +1,6 @@
 'use strict';
 import { jsdom } from 'jsdom';
+import glSupported from 'mapbox-gl-supported';
 
 const isNode = typeof window === 'undefined';
 const _ = isNode ? jsdom('<div id="site-canvas"></div>').defaultView : window;
@@ -8,4 +9,6 @@ if (isNode) {
   global.document = _.document;
   global.navigator = _.navigator;
 }
+const isSupported = !isNode && glSupported();
+export const mapboxgl = isSupported ? _.mapboxgl : () => true;
 export default _;
