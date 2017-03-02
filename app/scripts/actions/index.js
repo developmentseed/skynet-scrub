33800c65a3f00b1982a777dcbba47a7307e4e859
Drew Bollinger <drew@developmentseed.org>
2017-03-02 12:16:40 -0500

    Redo reducer without redux-undo

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -1,5 +1,7 @@
 export const UPDATE_SELECTION = 'UPDATE_SELECTION';
 export const UPDATE_SELECTION_HISTORY = 'UPDATE_SELECTION_HISTORY';
+export const UNDO = 'UNDO';
+export const REDO = 'REDO';
 
 export function updateSelection (selectionArray) {
   return { type: UPDATE_SELECTION, data: selectionArray };
@@ -7,4 +9,12 @@ export function updateSelection (selectionArray) {
 
 export function updateSelectionHistory () {
   return { type: UPDATE_SELECTION_HISTORY };
+}
+
+export function undo () {
+  return { type: UNDO };
+}
+
+export function redo () {
+  return { type: REDO };
 }
--- app/scripts/reducers/selection.js
+++ app/scripts/reducers/selection.js
@@ -1,17 +1,50 @@
-import undoable, { includeAction } from 'redux-undo';
-import { UPDATE_SELECTION_HISTORY, UPDATE_SELECTION } from '../actions';
+import hat from 'hat';
+import { UNDO, REDO, UPDATE_SELECTION_HISTORY, UPDATE_SELECTION } from '../actions';
 
-export const initial = [];
+const initial = {
+  past: [],
+  present: { historyId: 'initial', selection: [] },
+  future: []
+};
+
+const rack = hat.rack();
 
 const selection = (state = initial, action) => {
+  const { past, present, future } = state;
   switch (action.type) {
+    case UNDO:
+      const previous = past[past.length - 1];
+      const newPast = past.slice(0, past.length - 1);
+      return {
+        past: newPast,
+        present: previous,
+        future: [ present, ...future ]
+      };
+    case REDO:
+      const next = future[0];
+      const newFuture = future.slice(1);
+      return {
+        past: [ ...past, present ],
+        present: next,
+        future: newFuture
+      };
     case UPDATE_SELECTION:
-      return action.data;
+      // this doesn't update history
+      return {
+        past,
+        present: { historyId: state.present.historyId, selection: action.data },
+        future
+      };
     case UPDATE_SELECTION_HISTORY:
-      return [];
+      // this exclusively updates history (and clears present)
+      return {
+        past: [ ...past, present ],
+        present: { historyId: rack(), selection: [] },
+        future: []
+      };
     default:
       return state;
   }
 };
 
-export default undoable(selection, { filter: includeAction(UPDATE_SELECTION_HISTORY) });
+export default selection;
