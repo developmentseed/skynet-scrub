360cb2b1b4f72165ab66ff55812b84c8ee0d90ad
Drew Bollinger <drew@developmentseed.org>
2017-03-03 16:47:13 -0500

    Add documentation for updateSelection

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -4,6 +4,14 @@ export const REDO = 'REDO';
 export const COMPLETE_UNDO = 'COMPLETE_UNDO';
 export const COMPLETE_REDO = 'COMPLETE_REDO';
 
+/**
+ * Updates the selection store with a new array of changes
+ * @param {Array<Object>} selectionArray array of objects with the form:
+ *  { id, undo, redo } where `undo` and `redo` are GeoJSON features (or `null`)
+ *  describing the previous and current state respectively of the feature on the
+ *  map (at the time it was changed).
+ * @returns {Object} Redux action.
+ */
 export function updateSelection (selectionArray) {
   return { type: UPDATE_SELECTION, data: selectionArray };
 }
