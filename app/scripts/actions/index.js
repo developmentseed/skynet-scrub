ae2e1b38d049cee94b36d668e5850abd500c6788
Drew Bollinger <drew@developmentseed.org>
2017-03-04 16:44:49 -0500

    Track requested tiles

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -8,6 +8,7 @@ export const COMPLETE_UNDO = 'COMPLETE_UNDO';
 export const COMPLETE_REDO = 'COMPLETE_REDO';
 export const COMPLETE_MAP_UPDATE = 'COMPLETE_MAP_UPDATE';
 export const UPDATE_MAP_DATA = 'UPDATE_MAP_DATA';
+export const REQUEST_TILE = 'REQUEST_TILE';
 
 export function updateSelection (selectionArray) {
   return { type: UPDATE_SELECTION, data: selectionArray };
@@ -37,8 +38,19 @@ export function updateMapData (data) {
   return { type: UPDATE_MAP_DATA, data };
 }
 
+/**
+  * Tracks already requested tiles
+  * @param {string} tile of the form x/y/z
+  * @returns {Object} Redux action.
+  */
+
+export function requestTile (tile) {
+  return { type: REQUEST_TILE, data: tile };
+}
+
 export function fetchMapData (tile) {
   return (dispatch) => {
+    dispatch(requestTile(tile.join('/')));
     fetch(`${config.baseUrl}/features/${tile[2]}/${tile[0]}/${tile[1]}`)
       .then(response => response.json())
       .then(response => dispatch(updateMapData(response.objects)));
--- app/scripts/reducers/map.js
+++ app/scripts/reducers/map.js
@@ -1,15 +1,21 @@
-import { COMPLETE_MAP_UPDATE, UPDATE_MAP_DATA } from '../actions';
+import { COMPLETE_MAP_UPDATE, UPDATE_MAP_DATA, REQUEST_TILE } from '../actions';
 
 const initial = {
-  tempStore: null
+  tempStore: null,
+  requestedTiles: new Set()
 };
 
 const map = (state = initial, action) => {
   switch (action.type) {
     case COMPLETE_MAP_UPDATE:
-      return { tempStore: null };
+      return { tempStore: null, requestedTiles: state.requestedTiles };
     case UPDATE_MAP_DATA:
-      return { tempStore: action.data };
+      return { tempStore: action.data, requestedTiles: state.requestedTiles };
+    case REQUEST_TILE:
+      return {
+        tempStore: state.tempStore,
+        requestedTiles: new Set(state.requestedTiles).add(action.data, true)
+      };
     default:
       return state;
   }
