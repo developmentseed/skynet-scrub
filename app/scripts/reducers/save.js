1596a89bbd2fdad88fb78190ca8c819f349674ac
Derek Lieu <dl@dereklieu.com>
2017-03-11 17:40:22 +0000

    Create save reducer

---
+++ app/scripts/reducers/save.js
@@ -0,0 +1,17 @@
+import { SAVE } from '../actions';
+
+const initial = {
+  inflight: false,
+  error: null
+};
+
+const save = (state = initial, action) => {
+  switch (action.type) {
+    case SAVE:
+      return Object.assign({}, state, action.data);
+    default:
+      return state;
+  }
+};
+
+export default save;
