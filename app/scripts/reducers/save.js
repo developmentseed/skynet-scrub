f6a9fa96fbba81ccfb90c016340f72a53574b6de
Derek Lieu <dl@dereklieu.com>
2017-03-14 14:14:33 +0000

    Include local storage reducer in store

--- app/scripts/reducers/save.js
+++ app/scripts/reducers/save.js
@@ -1,16 +1,19 @@
-import { SAVE } from '../actions';
+import { SAVE, LOCAL_STORAGE } from '../actions';
 
 const initial = {
   inflight: false,
   error: null,
   success: null,
-  historyId: null
+  historyId: null,
+  cached: null
 };
 
 const save = (state = initial, action) => {
   switch (action.type) {
     case SAVE:
       return Object.assign({}, state, action.data);
+    case LOCAL_STORAGE:
+      return Object.assign({}, state, { cached: action.data });
     default:
       return state;
   }
