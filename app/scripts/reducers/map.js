c9b5389c16d9ced461841346f82f5ec6b9c6bf8f
sethvincent <sethvincent@gmail.com>
2017-03-21 15:00:07 -0400

    remove duplicate properties

--- app/scripts/reducers/map.js
+++ app/scripts/reducers/map.js
@@ -9,14 +9,13 @@ const initial = {
 const map = (state = initial, action) => {
   switch (action.type) {
     case COMPLETE_MAP_UPDATE:
-      return Object.assign({}, state, { tempStore: null, requestedTiles: state.requestedTiles });
+      return Object.assign({}, state, { requestedTiles: state.requestedTiles });
     case UPDATE_MAP_DATA:
-      return Object.assign({}, state, { tempStore: action.data, requestedTiles: state.requestedTiles });
+      return Object.assign({}, state, { tempStore: action.data });
     case TOGGLE_EXISTING_ROADS:
       return Object.assign({}, state, { showExistingRoads: !state.showExistingRoads });
     case REQUEST_TILE:
       return Object.assign({}, state, {
-        tempStore: state.tempStore,
         requestedTiles: new Set(state.requestedTiles).add(action.data, true)
       });
     default:
