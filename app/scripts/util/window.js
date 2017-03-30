7da57fb330bd9de7bd4b174d68d5fd8dd9863d06
Derek Lieu <dl@dereklieu.com>
2017-03-30 16:29:17 +0200

    Remove jsdom from window util

--- app/scripts/util/window.js
+++ app/scripts/util/window.js
@@ -1,14 +1,7 @@
 'use strict';
-import { jsdom } from 'jsdom';
 import check from 'mapbox-gl-supported';
-
 const isNode = typeof window === 'undefined';
-const _ = isNode ? jsdom('<div id="site-canvas"></div>').defaultView : window;
-if (isNode) {
-  global.window = _;
-  global.document = _.document;
-  global.navigator = _.navigator;
-}
+const _ = isNode ? {} : window;
 export const mapboxgl = !isNode ? _.mapboxgl : () => true;
 export const MapboxDraw = !isNode ? _.MapboxDraw : () => true;
 export const glSupport = check();
