b7b5d5b6f90c533453e88758db37870946b2ae90
Drew Bollinger <drew@developmentseed.org>
2017-06-22 17:16:26 -0400

    New port

--- app/scripts/config/base.js
+++ app/scripts/config/base.js
@@ -1,6 +1,6 @@
 module.exports = {
   environment: 'development',
-  baseUrl: 'http://138.197.97.15:4030',
+  baseUrl: 'http://138.197.97.15',
   existingRoadsSource: 'https://s3.amazonaws.com/vietbando/{z}/{x}/{y}.pbf',
   initialZoom: 14,
   minTileZoom: 13
--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   environment: 'production',
-  baseUrl: 'http://138.197.97.15:4030'
+  baseUrl: 'http://138.197.97.15'
 };
